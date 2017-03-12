8bc080d6479572a8818c83b3dff8652f2fef38f5
hutong <hutong0521@163.com>
2017-03-12 13:13:32 +0800

    自动加载第三方库

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
 
+include APPLICATION_PATH.'/vendor/autoload.php';
+
 $application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
