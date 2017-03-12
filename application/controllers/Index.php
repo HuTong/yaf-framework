16ef4859668cd1ed19e625a26ba4777b6ef3024f
hutong <hutong0521@163.com>
2017-03-12 14:20:41 +0800

    添加基础代码

---
+++ application/controllers/Index.php
@@ -0,0 +1,15 @@
+<?php
+class IndexController extends HuTong\Ycore\Xcontroller
+{
+
+	public function indexAction($name = "Stranger") 
+	{
+		//1. fetch query
+		$get = $this->getRequest()->getQuery("get", "default value");
+
+		$this->getView()->assign("name", $name);
+
+		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
+        return true;
+	}
+}
